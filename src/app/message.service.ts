cf57cad9d4c3642e815b70c5beff005a4c2b4cfc
RaphaelCF <44228816+RaphaelCF@users.noreply.github.com>
2018-10-25 11:57:51 +0200

    utilisation du snackbar mise en forme du log services

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,20 +1,22 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
 
-  constructor() { }
-	
+  constructor(public snackBar: MatSnackBar) { }
+
 	messages: string[] = [];
-	
+
 	add(message: string){
 		this.messages.push(message);
-	}
-	
+    this.snackBar.open(message, 'OK', {duration: 5000});
+  }
+
 	clear(){
 		this.messages = [];
 	}
-	
+
 }
